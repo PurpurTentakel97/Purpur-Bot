b88b0de588002dbe86d2e4039302fb52a4969e2c
Martin <martin.schreurs@netcologne.de>
2025-11-09 15:19:15 +0100

    fix: improve config loading

--- helpers/config.py
+++ helpers/config.py
@@ -1,81 +1,72 @@
-import json
+from dataclasses import fields, dataclass
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Self, final
 
+from helpers.file import LoadJsonResult, SaveJsonResult, load_json, save_json
 from helpers.log import log, LogLevel
 
+_CONFIG_SAMPLE_FILE: str = "config_sample.json"
+_CONFIG_FILE: str = "config.json"
 
+
+@final
+@dataclass(frozen=True)
 class Config:
-    def __init__(self, *, discord_token: str, twitch_client_id: str, twitch_credentials: str):
-        self._discord_token: str = discord_token
-        self._twitch_client_id: str = twitch_client_id
-        self._twitch_credentials: str = twitch_credentials
+    discord_token: str
+    twitch_client_id: str
+    twitch_credentials: str
 
-    @property
-    def discord_token(self) -> str:
-        return self._discord_token
+    @classmethod
+    def from_dict(cls, data: dict[str, str]) -> Self:
+        filtered_data: Dict[str, str] = {key: data[key].strip() for key in Config.get_fields()}
+        return cls(**filtered_data)
 
-    @property
-    def twitch_client_id(self) -> str:
-        return self._twitch_client_id
+    @classmethod
+    def get_fields(cls) -> list[str]:
+        return [f.name for f in fields(cls)]
 
-    @property
-    def twitch_credentials(self) -> str:
-        return self._twitch_credentials
+
+def _gen_default_config() -> Dict[str, str]:
+    config: Dict[str, str] = {}
+    for field in Config.get_fields():
+        config[field] = f"{field.upper()}"
+    return config
 
 
 def load_config() -> Config:
-    def load_single_config(path: Path) -> Dict[str, str] | None:
-        if not path.exists():
-            log(LogLevel.ERROR, f"Config file {path} does not exist")
-            return None
-
-        try:
-            with open(path, 'r', encoding='utf-8') as f:
-                return json.load(f)
-        except json.JSONDecodeError as e:
-            log(LogLevel.ERROR, f"Failed to parse config file {path}: {e}")
-            return None
-        except IOError as e:
-            log(LogLevel.ERROR, f"Failed to read config file {path}: {e}")
-            return None
-
-    def check_single_config_entry(config: Dict[str, str], config_sample: Dict[str, str], key: str) -> bool:
+    def _check_single_field(config: Dict[str, str], key: str) -> bool:
         if not key in config:
             log(LogLevel.ERROR, f"Config file does not contain required key {key}")
             return False
-        if not key in config_sample:
-            log(LogLevel.ERROR, f"Config sample file does not contain required key {key}")
+
+        if config[key].strip() == key.upper():
+            log(LogLevel.ERROR, f"Config entry {key} has default value {key.upper()}")
             return False
-        if config[key] == config_sample[key]:
-            log(LogLevel.ERROR, f"Config file key {key} is equal to config sample file key {key}")
-            return False
-        if config[key] == "":
-            log(LogLevel.ERROR, f"Config file key {key} is empty")
+
+        if config[key].strip() == "":
+            log(LogLevel.ERROR, f"Config entry {key} is empty")
+
         return True
 
-    config: Dict[str, str] | None = load_single_config(Path.cwd() / "config.json")
-    config_sample: Dict[str, str] | None = load_single_config(Path.cwd() / "config_sample.json")
+    config_sample_result: LoadJsonResult = load_json(Path.cwd() / _CONFIG_SAMPLE_FILE)
+    if not config_sample_result.success:
+        log(LogLevel.INFO, "Config sample file does not exist, creating it")
+        result: SaveJsonResult = save_json(Path.cwd() / _CONFIG_SAMPLE_FILE, _gen_default_config())
+        if not result.success:
+            log(LogLevel.CRITICAL, "Failed to create a config sample file")
+            raise Exception("Failed to create a config sample file")
 
-    if config is None or config_sample is None:
-        log(LogLevel.CRITICAL, "Failed to load config")
-        raise Exception("Failed to load config")
+    config_result: LoadJsonResult = load_json(Path.cwd() / _CONFIG_FILE)
+    if not config_result.success:
+        log(LogLevel.CRITICAL, "Failed to load the config file")
+        raise Exception("Failed to load the config file")
 
-    if not check_single_config_entry(config,
-                                     config_sample,
-                                     "discord_token"):
-        raise Exception("Faulty Discord Token")
+    valid_data: bool = True
+    for key in Config.get_fields():
+        valid_data = valid_data and _check_single_field(config_result.data, key)
 
-    if not check_single_config_entry(config,
-                                     config_sample,
-                                     "twitch_client_id"):
-        raise Exception("Faulty Twitch Client ID")
+    if not valid_data:
+        log(LogLevel.CRITICAL, "Invalid data in config file")
+        raise Exception("Invalid data in config file")
 
-    if not check_single_config_entry(config,
-                                     config_sample,
-                                     "twitch_credentials"):
-        raise Exception("Faulty Twitch Credentials")
-
-    return Config(discord_token=config["discord_token"],
-                  twitch_client_id=config["twitch_client_id"],
-                  twitch_credentials=config["twitch_credentials"])
+    return Config.from_dict(config_result.data)
