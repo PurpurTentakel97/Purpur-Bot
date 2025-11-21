45217852b5a7f61fc5b7f2bdf953fb4b9fb959f7
Martin <martin.schreurs@netcologne.de>
2025-11-21 23:04:18 +0100

    fix: use the new logging functions in the general program

--- helpers/config.py
+++ helpers/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import Dict, Self, final
 
 from helpers.file import LoadJsonResult, SaveJsonResult, load_json, save_json
-from helpers.log import log, LogLevel
+from helpers.log import log_general, LogLevel
 
 _CONFIG_SAMPLE_FILE: str = "config_sample.json"
 _CONFIG_FILE: str = "config.json"
@@ -36,29 +36,29 @@ def _gen_default_config() -> Dict[str, str]:
 def load_config() -> Config:
     def _check_single_field(config: Dict[str, str], key: str) -> bool:
         if not key in config:
-            log(LogLevel.ERROR, f"Config file does not contain required key {key}")
+            log_general(LogLevel.ERROR, f"Config file does not contain required key {key}")
             return False
 
         if config[key].strip() == key.upper():
-            log(LogLevel.ERROR, f"Config entry {key} has default value {key.upper()}")
+            log_general(LogLevel.ERROR, f"Config entry {key} has default value {key.upper()}")
             return False
 
         if config[key].strip() == "":
-            log(LogLevel.ERROR, f"Config entry {key} is empty")
+            log_general(LogLevel.ERROR, f"Config entry {key} is empty")
 
         return True
 
     config_sample_result: LoadJsonResult = load_json(Path.cwd() / _CONFIG_SAMPLE_FILE)
     if not config_sample_result.success:
-        log(LogLevel.INFO, "Config sample file does not exist, creating it")
+        log_general(LogLevel.INFO, "Config sample file does not exist, creating it")
         result: SaveJsonResult = save_json(Path.cwd() / _CONFIG_SAMPLE_FILE, _gen_default_config())
         if not result.success:
-            log(LogLevel.CRITICAL, "Failed to create a config sample file")
+            log_general(LogLevel.CRITICAL, "Failed to create a config sample file")
             raise Exception("Failed to create a config sample file")
 
     config_result: LoadJsonResult = load_json(Path.cwd() / _CONFIG_FILE)
     if not config_result.success:
-        log(LogLevel.CRITICAL, "Failed to load the config file")
+        log_general(LogLevel.CRITICAL, "Failed to load the config file")
         raise Exception("Failed to load the config file")
 
     valid_data: bool = True
@@ -66,7 +66,7 @@ def load_config() -> Config:
         valid_data = valid_data and _check_single_field(config_result.data, key)
 
     if not valid_data:
-        log(LogLevel.CRITICAL, "Invalid data in config file")
+        log_general(LogLevel.CRITICAL, "Invalid data in config file")
         raise Exception("Invalid data in config file")
 
     return Config.from_dict(config_result.data)
