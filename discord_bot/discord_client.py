802377ba4543846e9ff4e5a066d95377ea9827ab
Martin <martin.schreurs@netcologne.de>
2025-11-21 22:44:31 +0100

    feat: use new logging functions in discord bot

--- discord_bot/discord_client.py
+++ discord_bot/discord_client.py
@@ -1,5 +1,7 @@
 import discord
 from discord import Client
+from helpers.log import log_discord, LogLevel, log_exception, LogProgramm
+import sys
 
 class DiscordClient(Client):
     def __init__(self,
@@ -7,20 +9,26 @@ class DiscordClient(Client):
                  intents: discord.Intents):
         super().__init__(intents=intents)
 
-    async def send_message(self, channel_id, message):
+    async def send_message(self, channel_id: int, message: str) -> None:
         channel = self.get_channel(channel_id)
+        if not channel:
+            log_discord(LogLevel.ERROR, f"Channel {channel_id} not found!")
+            return
         await channel.send(message)
 
+    async def on_ready(self) -> None:
+        log_discord(LogLevel.INFO, "Discord client is ready!")
 
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
-
-
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message) -> None:
         if message.author == self.user:
             return
 
-        print(f'{message.author} said: {message.content}')
+        log_discord(LogLevel.DEBUG, f"{message.author}: {message.content}")
 
-    async def on_error(self, event, *args, **kwargs):
-        print(f'Error on {event}: {args} {kwargs}')
+    async def on_error(self, event, *args, **kwargs) -> None:
+        exc_type, value, traceback = sys.exc_info()
+
+        if value:
+            log_exception(value, LogProgramm.Discord, f"Error in event: {event}")
+        else:
+            log_discord(LogLevel.ERROR, f"Discord client error: {event} {args} {kwargs}")
