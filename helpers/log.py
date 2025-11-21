c05254dc58a9da2dfad221c403acffdd574a8a79
Martin <martin.schreurs@netcologne.de>
2025-11-21 22:44:15 +0100

    feat: add new logging functions

--- helpers/log.py
+++ helpers/log.py
@@ -1,13 +1,12 @@
-from enum import Enum
+from enum import Enum, auto
 from datetime import datetime
 from types import TracebackType
 
 
-class LogLevel(Enum):
-    INFO = 0
-    DEBUG = 1
-    ERROR = 2
-    CRITICAL = 3
+class LogProgramm(Enum):
+    General = auto()
+    Discord = auto()
+    Twitch = auto()
 
     def __str__(self):
         return self.name
@@ -15,12 +14,31 @@ class LogLevel(Enum):
     def __repr__(self):
         return self.name
 
+class LogLevel(Enum):
+    INFO = auto()
+    DEBUG = auto()
+    ERROR = auto()
+    CRITICAL = auto()
 
-def log(level: LogLevel, message: str) -> None:
-    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] | {level:8} | {message}")
+    def __str__(self):
+        return self.name
 
+    def __repr__(self):
+        return self.name
 
-def log_exception(exception: Exception, message: str) -> None:
+def log(level: LogLevel, programm: LogProgramm, message: str) -> None:
+    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] | {level:8} | {programm:7} | {message}")
+
+def log_general(level: LogLevel, message: str) -> None:
+    log(level,LogProgramm.General, message)
+
+def log_discord(level: LogLevel, message: str) -> None:
+    log(level,LogProgramm.Discord, message)
+
+def log_twitch(level: LogLevel, message: str) -> None:
+    log(level,LogProgramm.Twitch, message)
+
+def log_exception(exception: Exception, programm: LogProgramm, message: str) -> None:
     exception_name: str = type(exception).__name__
     trace_back: TracebackType | None = exception.__traceback__
     location: str = "unknown location"
@@ -33,4 +51,5 @@ def log_exception(exception: Exception, message: str) -> None:
         location = f"{filename}:{line_number} in {function_name}()"
 
     log(LogLevel.CRITICAL,
+        programm,
         f"{exception_name} | {location} | {message}")
