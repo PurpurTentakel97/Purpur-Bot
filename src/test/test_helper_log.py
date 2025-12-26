583c2d610c0cb1793aa2f00d5fa04428a990da30
Martin <martin.schreurs@netcologne.de>
2025-12-26 10:35:01 +0100

    test: log tests for log level and programm

--- src/test/test_helper_log.py
+++ src/test/test_helper_log.py
@@ -41,17 +41,17 @@ def test_output_format(capsys: pytest.CaptureFixture[str]) -> None:
     assert re.search(regex, captured.out, re.MULTILINE) is not None
 
 
-def _log_test_default(config_level: LogLevel, call_level: LogLevel, message: str) -> None:
+def _log_call_default(config_level: LogLevel, call_level: LogLevel, message: str) -> None:
     LogLevelConfig.default.level = config_level
     log_default(call_level, message)
 
 
-def _log_test_discord(config_level: LogLevel, call_level: LogLevel, message: str) -> None:
+def _log_call_discord(config_level: LogLevel, call_level: LogLevel, message: str) -> None:
     LogLevelConfig.discord.level = config_level
     log_discord(call_level, message)
 
 
-def _log_test_twitch(config_level: LogLevel, call_level: LogLevel, message: str) -> None:
+def _log_call_twitch(config_level: LogLevel, call_level: LogLevel, message: str) -> None:
     LogLevelConfig.twitch.level = config_level
     log_twitch(call_level, message)
 
@@ -62,7 +62,7 @@ def _log_test_twitch(config_level: LogLevel, call_level: LogLevel, message: str)
 @pytest.mark.parametrize(
     "call_level", [LogLevel.DEBUG, LogLevel.INFO, LogLevel.WARNING, LogLevel.ERROR, LogLevel.CRITICAL]
 )
-@pytest.mark.parametrize("log_function", [_log_test_default, _log_test_discord, _log_test_twitch])
+@pytest.mark.parametrize("log_function", [_log_call_default, _log_call_discord, _log_call_twitch])
 def test_should_not_log_below_level(
     capsys: pytest.CaptureFixture[str],
     config_level: LogLevel,
@@ -81,3 +81,34 @@ def test_should_not_log_below_level(
         assert test_message in captured.out
     else:
         assert captured.out == ""
+
+
+@pytest.mark.parametrize(
+    "log_level, expected_string",
+    [
+        (LogLevel.DEBUG, "DEBUG"),
+        (LogLevel.INFO, "INFO"),
+        (LogLevel.WARNING, "WARNING"),
+        (LogLevel.ERROR, "ERROR"),
+        (LogLevel.CRITICAL, "CRITICAL"),
+    ],
+)
+def test_correct_log_level(capsys: pytest.CaptureFixture[str], log_level: LogLevel, expected_string: str) -> None:
+    _reset_log()
+
+    log_default(log_level, "test message")
+    captured: CaptureResult[str] = capsys.readouterr()
+    assert expected_string in captured.out
+
+
+@pytest.mark.parametrize(
+    "log_function, expected_string", [(log_default, "Default"), (log_discord, "Discord"), (log_twitch, "Twitch")]
+)
+def test_correct_log_program(
+    capsys: pytest.CaptureFixture[str], log_function: Callable[[LogLevel, str], None], expected_string: str
+) -> None:
+    _reset_log()
+
+    log_function(LogLevel.DEBUG, "test message")
+    captured: CaptureResult[str] = capsys.readouterr()
+    assert expected_string in captured.out
