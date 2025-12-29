3a08e577a02e4d00a29f56bf9f4b074df2f61886
Martin <martin.schreurs@netcologne.de>
2025-12-29 22:26:27 +0100

    feat: json config for users

--- src/bot/types/programm_parts.py
+++ src/bot/types/programm_parts.py
@@ -3,6 +3,7 @@ from typing import Optional
 from attr import dataclass
 
 from bot.discord_bot.discord_client import DiscordClient
+from bot.helpers.config import ProgrammConfig
 from bot.twitch_bot.twitch_client import TwitchClient
 
 
@@ -10,3 +11,4 @@ from bot.twitch_bot.twitch_client import TwitchClient
 class ProgramParts:
     discord: Optional[DiscordClient]
     twitch: Optional[TwitchClient]
+    config: Optional[ProgrammConfig]
