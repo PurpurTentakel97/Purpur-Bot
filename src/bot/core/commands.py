13e41557b8f9db4a263f664e657eb70cec33f1d5
Martin <martin.schreurs@netcologne.de>
2025-12-30 22:47:48 +0100

    feat: first command layout

--- src/bot/core/commands.py
+++ src/bot/core/commands.py
@@ -1,37 +1,54 @@
+from typing import Optional
 from bot.helpers.log import log_default, LogLevel
 from bot.types.chat_message import ChatMessage
+from bot.types.response_message import ResponseMessage
 
-def add_command(id_: int, command_name: str, command_message: str) -> None:
+
+def add_command(id_: int, command_name: str, command_message: str) -> ResponseMessage:
     log_default(
         LogLevel.DEBUG, f"command '{command_name}' added: '{command_message}' to id {id_}"
     )
+    return ResponseMessage()
 
-def edit_command(id_: int, command_name: str, command_message: str) -> None:
+
+def edit_command(id_: int, command_name: str, command_message: str) -> ResponseMessage:
     log_default(
         LogLevel.DEBUG, f"command '{command_name}' edited: '{command_message}' with id {id_}"
     )
+    return ResponseMessage()
 
-def remove_command(id_: int, command_name: str) -> None:
+
+def remove_command(id_: int, command_name: str) -> ResponseMessage:
     log_default(LogLevel.DEBUG, f"command '{command_name}' removed with id {id_}")
+    return ResponseMessage()
 
-def handle_command(message: ChatMessage) -> None:
+
+def try_lookup_command(id_: int, command_name: str) -> Optional[ResponseMessage]:
+    return None
+
+
+def handle_command(message: ChatMessage) -> Optional[ResponseMessage]:
     parts = message.text.strip().split(" ")
-    match parts:
-        case ["!command", "add", command_name, *msg]:
-            command_message = " ".join(msg)
-            if not command_message:
-                return
-            add_command(message.id_, command_name, command_message)
 
-        case ["!command", "edit", command_name, *msg]:
-            command_message = " ".join(msg)
-            if not command_message:
-                return
-            edit_command(message.id_, command_name, command_message)
+    if True:
+        # todo: restrict by mod and admin
+        match parts:
+            case ["!command", "add", command_name, *msg]:
+                command_message = " ".join(msg)
+                if not command_message:
+                    return ResponseMessage()
+                return add_command(message.id_, command_name, command_message)
 
-        case ["!command", "remove", command_name]:
-            remove_command(message.id_, command_name)
+            case ["!command", "edit", command_name, *msg]:
+                command_message = " ".join(msg)
+                if not command_message:
+                    return ResponseMessage()
+                return edit_command(message.id_, command_name, command_message)
 
-        case _:
-            # todo: look up command
-            return
+            case ["!command", "remove", command_name]:
+                return remove_command(message.id_, command_name)
+
+            case _:
+                pass
+
+    return try_lookup_command(message.id_, parts[0])
---
+++ src/bot/types/response_message.py
@@ -0,0 +1,9 @@
+from typing import final
+
+from attr import dataclass
+
+
+@final
+@dataclass
+class ResponseMessage:
+    pass
