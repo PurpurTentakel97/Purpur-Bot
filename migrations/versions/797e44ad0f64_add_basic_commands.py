20e041503ca1fe12b8de8133c05d323738e00435
Martin <martin.schreurs@netcologne.de>
2025-12-31 23:59:49 +0100

    db: basic command tabel

--- migrations/versions/797e44ad0f64_add_basic_commands.py
+++ migrations/versions/797e44ad0f64_add_basic_commands.py
@@ -5,23 +5,26 @@ Revises:
 Create Date: 2025-12-31 21:56:05.525401
 
 """
-from typing import Sequence, Union
 
-from alembic import op
+from collections.abc import Sequence
+
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '797e44ad0f64'
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "797e44ad0f64"
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
-    op.create_table("basic_commands",
-                    sa.Column("id", sa.Integer, primary_key=True),
-                    sa.Column("name", sa.String, nullable=False, primary_key=True),
-                    sa.Column("response", sa.String, nullable=False))
+    op.create_table(
+        "basic_commands",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.String, nullable=False, primary_key=True),
+        sa.Column("message", sa.String, nullable=False),
+    )
 
 
 def downgrade() -> None:
