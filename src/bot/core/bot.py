b8327e86b9d450d9d0b9a841064c55649b7925e6
Martin <martin.schreurs@netcologne.de>
2026-01-09 00:07:59 +0100

    add core: twitch

--- src/bot/core/bot.py
+++ src/bot/core/bot.py
@@ -1,7 +1,6 @@
-from typing import Optional
-
 from bot.chat.on_demand import stop_all_discord_bots_from_bot
 from bot.chat.on_demand import stop_all_twitch_bots_from_bot
+from bot.core.types.result import Result
 from bot.database.bot import FIELD_NAME
 from bot.database.bot import delete_bot as delete_bot_db
 from bot.database.bot import insert_bot as insert_bot_db
@@ -11,23 +10,23 @@ from bot.database.bot import update_bot as update_bot_db
 from bot.database.types.bot_config import BotConfigDB
 
 
-def get_bot(bot_id: int) -> Optional[BotConfigDB]:
+def get_bot(bot_id: int) -> Result[BotConfigDB]:
     return select_bot_db(bot_id)
 
 
-def get_bots_by_twitch_id(twitch_id: str) -> list[BotConfigDB]:
+def get_bots_by_twitch_id(twitch_id: str) -> Result[list[BotConfigDB]]:
     return select_bots_by_twitch_id_db(twitch_id)
 
 
-def add_bot(twitch_id: str) -> Optional[int]:
+def add_bot(twitch_id: str) -> Result[int]:
     return insert_bot_db(twitch_id)
 
 
-def update_bot_name(bot_id: int, name: str) -> bool:
+def update_bot_name(bot_id: int, name: str) -> Result[None]:
     return update_bot_db(bot_id, {FIELD_NAME: name})
 
 
-async def delete_bot(bot_id: int) -> bool:
+async def delete_bot(bot_id: int) -> Result[None]:
     await stop_all_twitch_bots_from_bot(bot_id)
     await stop_all_discord_bots_from_bot(bot_id)
 
---
+++ src/bot/core/twitch.py
@@ -0,0 +1,32 @@
+from bot.chat.on_demand import start_single_twitch_bot
+from bot.chat.on_demand import stop_single_twitch_bot
+from bot.core.types.result import Result
+from bot.core.types.result import ResultState
+from bot.database.twitch import delete_twitch_channel as delete_twitch_channel_db
+from bot.database.twitch import insert_twitch_channel as insert_twitch_channel_db
+
+
+async def add_twitch_channel(bot_id: int, channel: str) -> Result[int]:
+    insert_result = insert_twitch_channel_db(bot_id, channel)
+
+    if not insert_result.state.is_success():
+        return insert_result
+
+    start_result = await start_single_twitch_bot(bot_id, channel)
+
+    if not start_result:
+        delete_twitch_channel_db(bot_id, channel)
+        return Result(ResultState.ERROR, None)
+
+    return insert_result
+
+
+async def delete_twitch_channel(bot_id: int, channel: str) -> Result[None]:
+    stop_result = await stop_single_twitch_bot(bot_id, channel)
+
+    if not stop_result:
+        return Result(ResultState.ERROR, None)
+
+    delete_result = delete_twitch_channel_db(bot_id, channel)
+
+    return delete_result
