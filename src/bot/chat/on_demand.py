77d730c69574c673dd10f8df7b557fb42ecd340a
Martin <martin.schreurs@netcologne.de>
2026-01-09 23:34:08 +0100

    fix: remove discord bots from server while leaving

--- src/bot/chat/on_demand.py
+++ src/bot/chat/on_demand.py
@@ -3,7 +3,7 @@ from bot.chat.twitch_chat import TwitchChat
 from bot.core.types.programm_parts import PROGRAMM_PARTS
 
 
-async def start_single_discord_bot(id_: int, server_id: int) -> bool:
+def start_single_discord_bot(id_: int, server_id: int) -> bool:
     if PROGRAMM_PARTS.discord is None:
         return False
 
@@ -33,6 +33,7 @@ async def stop_all_discord_bots_from_bot(bot_id: int) -> None:
     for server in PROGRAMM_PARTS.discord.servers:
         if server.bot_id == bot_id:
             PROGRAMM_PARTS.discord.remove_server(server)
+            await PROGRAMM_PARTS.discord.leave_guild(server.server_id)
 
 
 async def start_single_twitch_bot(id_: int, channel_name: str) -> bool:
