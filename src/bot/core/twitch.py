bebcd09642b50a1118b0b58f96d46a7fae46ab3e
Martin <martin.schreurs@netcologne.de>
2026-01-11 23:10:42 +0100

    fix: ensure good error message when adding an existing bot

--- src/bot/core/twitch.py
+++ src/bot/core/twitch.py
@@ -3,18 +3,32 @@ from bot.chat.on_demand import stop_single_twitch_bot
 from bot.core.helpers.string import identifier_for_db
 from bot.core.types.result import Result
 from bot.core.types.result import ResultState
+from bot.database.twitch import FIELD_CHANNEL_NAME
 from bot.database.twitch import delete_twitch_channel as delete_twitch_channel_db
 from bot.database.twitch import insert_twitch_channel as insert_twitch_channel_db
+from bot.database.twitch import select_twitch_channel_by as select_twitch_channel_by_db
 from bot.database.twitch import select_twitch_channels_by_bot_id as select_twitch_channels_db
 from bot.database.types.twitch_channel import TwitchChannelDB
+
+
+def _exists(channel: str) -> bool:
+    return get_twitch_channel_by_name(channel).state.success
 
 
 def get_twitch_channels_from_bot(bot_id: int) -> Result[list[TwitchChannelDB]]:
     return select_twitch_channels_db(bot_id)
 
 
+def get_twitch_channel_by_name(channel_name: str) -> Result[TwitchChannelDB]:
+    return select_twitch_channel_by_db({FIELD_CHANNEL_NAME: channel_name})
+
+
 async def add_twitch_channel(bot_id: int, channel: str) -> Result[int]:
     channel_db = identifier_for_db(channel)
+
+    if _exists(channel_db):
+        return Result(ResultState.ALREADY_EXISTS, None)
+
     insert_result = insert_twitch_channel_db(bot_id, channel_db)
 
     if insert_result.state.fail:
--- src/bot/database/discord.py
+++ src/bot/database/discord.py
@@ -1,12 +1,19 @@
+from typing import Any
+
 from bot.core.types.programm_parts import PROGRAMM_PARTS
 from bot.core.types.result import Result
 from bot.database.types.discord_server import DiscordServerDB
 
 TABLE_NAME = "bot_discord_lookup"
+FIELD_SERVER_ID = "server_id"
 
 
 def select_discord_servers_by_bot_id(bot_id: int) -> Result[list[DiscordServerDB]]:
     return PROGRAMM_PARTS.database.select_all(table_name=TABLE_NAME, where={"bot_id": bot_id}, type_=DiscordServerDB)
+
+
+def select_discord_by(where: dict[str, Any]) -> Result[DiscordServerDB]:
+    return PROGRAMM_PARTS.database.select_one(table_name=TABLE_NAME, where=where, type_=DiscordServerDB)
 
 
 def insert_discord_server(bot_id: int, server_id: int, server_name: str) -> Result[int]:
