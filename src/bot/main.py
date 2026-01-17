e0d830de94beef11798a00668f31c9b6a3641c0a
Martin <martin.schreurs@netcologne.de>
2026-01-17 19:26:28 +0100

    frontend: remove unused routes and related imports

--- src/bot/main.py
+++ src/bot/main.py
@@ -9,14 +9,10 @@ from fastapi.staticfiles import StaticFiles
 from bot.chat.message_handler import handle_messages
 from bot.core.startup import startup_programm
 from bot.core.terminate import terminate_programm
-from bot.frontend.routes.api_alias import router as api_alias_router
-from bot.frontend.routes.api_bot import router as api_bot_router
-from bot.frontend.routes.api_commands import router as api_commands_router
-from bot.frontend.routes.api_counter import router as api_counter_router
 from bot.frontend.routes.api_auth import router as auth_router
-from bot.frontend.routes.dashboard import router as dashboard_router
-from bot.frontend.routes.home import router as home_router
+from bot.frontend.routes.api_bot import router as api_bot_router
 from bot.frontend.routes.api_icons import router as icon_router
+from bot.frontend.routes.home import router as home_router
 
 
 @asynccontextmanager
@@ -39,11 +35,7 @@ async def main(_: FastAPI) -> AsyncGenerator[None]:
 
 app: Final = FastAPI(lifespan=main)
 app.mount("/static", StaticFiles(directory="src/bot/frontend/static"), name="static")
-app.include_router(api_alias_router)
 app.include_router(api_bot_router)
-app.include_router(api_commands_router)
-app.include_router(api_counter_router)
 app.include_router(auth_router)
-app.include_router(dashboard_router)
 app.include_router(home_router)
 app.include_router(icon_router)
