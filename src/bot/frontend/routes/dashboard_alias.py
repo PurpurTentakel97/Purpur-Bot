9056893f2d78d29f0e9847b1fee03d94a5e991c4
Martin <martin.schreurs@netcologne.de>
2026-01-19 20:39:17 +0100

    frontend: add dashboard alias routes

---
+++ src/bot/frontend/routes/dashboard_alias.py
@@ -0,0 +1,139 @@
+from http import HTTPStatus
+from typing import Annotated
+from typing import Optional
+
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import Form
+from fastapi.responses import RedirectResponse
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.templating import Jinja2Templates
+
+from bot.core.alias_dict import add_alias as add_alias_core
+from bot.core.alias_dict import delete_alias_by_id as delete_alias_by_id_core
+from bot.core.alias_dict import edit_dict_alias_by_id as edit_dict_alias_by_id_core
+from bot.core.alias_dict import edit_dict_explanation_by_id as edit_dict_explanation_by_id_core
+from bot.core.alias_dict import get_alias_by_id as get_alias_by_id_core
+from bot.core.alias_dict import select_dict_from_bot as select_dict_from_bot_core
+from bot.core.types.result import Result
+from bot.database.types.alias_dict_entry import AliasDictEntry
+from bot.database.types.bot_config import BotConfigDB
+from bot.frontend.helpers.auth import get_authenticated_twitch_user
+from bot.frontend.helpers.auth import get_discord_user
+from bot.frontend.helpers.route_utils import get_templates
+from bot.frontend.helpers.route_utils import get_valid_bot
+from bot.frontend.types.discord_user_info import DiscordUserInfo
+from bot.frontend.types.twitch_user_info import TwitchUserInfo
+
+router = APIRouter(prefix="/dashboard/alias", dependencies=[Depends(get_authenticated_twitch_user)])
+
+
+@router.get("/{bot_id:int}")
+async def dashboard_alias(
+    request: Request,
+    bot: Annotated[BotConfigDB, Depends(get_valid_bot)],
+    template: Annotated[Jinja2Templates, Depends(get_templates)],
+    twitch_user: Annotated[TwitchUserInfo, Depends(get_authenticated_twitch_user)],
+    discord_user: Annotated[Optional[DiscordUserInfo], Depends(get_discord_user)],
+) -> Response:
+    aliases = select_dict_from_bot_core(bot.id)
+    if aliases.value is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Aliases not found")
+
+    return template.TemplateResponse(
+        request=request,
+        name="dashboard_alias.html",
+        context={
+            "bot": bot,
+            "twitch_user": twitch_user,
+            "discord_user": discord_user,
+            "aliases": aliases.value,
+        },
+    )
+
+
+@router.post("/{bot_id:int}")
+async def dashboard_alias_add(
+    bot: Annotated[BotConfigDB, Depends(get_valid_bot)],
+    alias: Annotated[str, Form()],
+    explanation: Annotated[str, Form()],
+) -> RedirectResponse:
+    result = add_alias_core(bot.id, alias, explanation)
+
+    if result.state.fail:
+        return RedirectResponse(
+            url=f"/dashboard/alias/{bot.id}?error_message=Failed to save alias | reason: {result.state.name}",
+            status_code=HTTPStatus.SEE_OTHER,
+        )
+
+    return RedirectResponse(
+        url=f"/dashboard/alias/{bot.id}?success_message=Alias saved successfully", status_code=HTTPStatus.SEE_OTHER
+    )
+
+
+@router.post("/{entry_id:int}/alias")
+async def dashboard_alias_update_name(
+    entry: Annotated[Result[AliasDictEntry], Depends(get_alias_by_id_core)], alias: Annotated[str, Form()]
+) -> RedirectResponse:
+    if entry.value is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Alias not found")
+
+    result = edit_dict_alias_by_id_core(entry.value.id, alias)
+
+    if result.state.fail:
+        return RedirectResponse(
+            url=f"/dashboard/alias/{entry.value.bot_id}?error_message=Failed to update alias "
+            + f"| reason: {result.state.name}",
+            status_code=HTTPStatus.SEE_OTHER,
+        )
+
+    return RedirectResponse(
+        url=f"/dashboard/alias/{entry.value.bot_id}?success_message=Alias updated successfully",
+        status_code=HTTPStatus.SEE_OTHER,
+    )
+
+
+@router.post("/{entry_id:int}/explanation")
+async def dashboard_alias_update_explanation(
+    entry: Annotated[Result[AliasDictEntry], Depends(get_alias_by_id_core)], explanation: Annotated[str, Form()]
+) -> RedirectResponse:
+    if entry.value is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Alias not found")
+
+    result = edit_dict_explanation_by_id_core(entry.value.id, explanation)
+
+    if result.state.fail:
+        return RedirectResponse(
+            url=f"/dashboard/alias/{entry.value.bot_id}?error_message=Failed to update explanation "
+            + f"| reason: {result.state.name}",
+            status_code=HTTPStatus.SEE_OTHER,
+        )
+
+    return RedirectResponse(
+        url=f"/dashboard/alias/{entry.value.bot_id}?success_message=Explanation updated successfully",
+        status_code=HTTPStatus.SEE_OTHER,
+    )
+
+
+@router.post("/{entry_id:int}/delete")
+async def dashboard_alias_delete(
+    entry: Annotated[Result[AliasDictEntry], Depends(get_alias_by_id_core)],
+) -> RedirectResponse:
+    if entry.value is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Alias not found")
+
+    result = delete_alias_by_id_core(entry.value.id)
+
+    if result.state.fail:
+        return RedirectResponse(
+            url=f"/dashboard/alias/{entry.value.bot_id}?error_message=Failed to delete alias "
+            + f"| reason: {result.state.name}",
+            status_code=HTTPStatus.SEE_OTHER,
+        )
+
+    return RedirectResponse(
+        url=f"/dashboard/alias/{entry.value.bot_id}?success_message=Alias deleted successfully",
+        status_code=HTTPStatus.SEE_OTHER,
+    )
--- src/bot/main.py
+++ src/bot/main.py
@@ -17,6 +17,7 @@ from bot.frontend.helpers.auth import get_twitch_user
 from bot.frontend.helpers.route_utils import get_templates
 from bot.frontend.routes.api_auth import router as auth_router
 from bot.frontend.routes.api_icons import router as icon_router
+from bot.frontend.routes.dashboard_alias import router as dashboard_alias_router
 from bot.frontend.routes.dashboard_commands import router as dashboard_commands_router
 from bot.frontend.routes.dashboard_counter import router as dashboard_counter_router
 from bot.frontend.routes.dashboard_discord import router as dashboard_discord_router
@@ -54,6 +55,7 @@ app.include_router(dashboard_main_router)
 app.include_router(dashboard_twitch_router)
 app.include_router(dashboard_discord_router)
 app.include_router(dashboard_commands_router)
+app.include_router(dashboard_alias_router)
 app.include_router(dashboard_counter_router)
 
 
