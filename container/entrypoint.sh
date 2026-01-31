9913333ef50cd11e29fc0f772af97e0d7f296bd4
Martin <martin.schreurs@netcologne.de>
2026-01-31 14:08:54 +0100

    feat: first running container

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
-uv run --no-dev uvicorn --host 0.0.0.0 --port 8080 bot.main:app
-# uv run --no-dev src/bot/main.py
-# python src/bot/main.py
-sleep 1d
+uv run --no-dev alembic upgrade head
+uv run --no-dev uvicorn --host 0.0.0.0 --port 8000 bot.main:app
