d60da15ae90d4220b95b10e253f7be538b93ac34
Martin <martin.schreurs@netcologne.de>
2026-02-28 18:23:43 +0100

    fix: use available properties instead of isinstance checks

--- src/bot/chat/types/message.py
+++ src/bot/chat/types/message.py
@@ -6,8 +6,6 @@ from attr import dataclass
 from discord import Message as DiscordMessage
 from twitchAPI.chat import ChatMessage as TwitchChatMessage
 
-from bot.chat.discord_server import DiscordServer
-from bot.chat.twitch_chat import TwitchChat
 from bot.chat.types.message_response import ChatMessageResponse
 from bot.core.types.permission_level import PermissionLevel
 
@@ -30,8 +28,8 @@ class ChatMessage:
 
     @property
     def has_twitch_message(self) -> bool:
-        return isinstance(self.sender_chat, TwitchChat)
+        return self.sender_chat.is_twitch
 
     @property
     def has_discord_message(self) -> bool:
-        return isinstance(self.sender_chat, DiscordServer)
+        return self.sender_chat.is_discord
