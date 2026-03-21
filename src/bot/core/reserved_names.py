e5a93b93bc82ecb1362a7c2bf050f278b79fbafc
Martin <martin.schreurs@netcologne.de>
2026-03-21 14:31:09 +0100

    feat: add "help" to reserved names list

--- src/bot/core/reserved_names.py
+++ src/bot/core/reserved_names.py
@@ -1,1 +1,1 @@
-RESERVED_NAMES = {"title", "game", "tags", "com", "counter", "alias", "coms", "quote"}
+RESERVED_NAMES = {"title", "game", "tags", "com", "counter", "alias", "coms", "quote", "help"}
