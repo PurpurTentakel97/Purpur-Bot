2a7ebe2a5cd1f328396a6f1d1cf7b467da5f7725
martin <martin.schreurs@netcolone.de>
2026-05-31 15:10:41 +0200

    db: add command permission level incl. default value

---
+++ migrations/versions/2fa88f04dc53_add_command_permission.py
@@ -0,0 +1,27 @@
+"""command permission
+
+Revision ID: 2fa88f04dc53
+Revises: ec255c4d579a
+Create Date: 2026-05-31 14:37:16.028431
+
+"""
+from typing import Sequence, Union
+from bot.core.types.permission_level import PermissionLevel
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2fa88f04dc53'
+down_revision: Union[str, Sequence[str], None] = 'ec255c4d579a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("basic_commands", sa.Column("permission_level", sa.Integer(), nullable=False, server_default=str(int(PermissionLevel.USER))))
+
+
+def downgrade() -> None:
+    op.drop_column("basic_commands", "permission_level")
